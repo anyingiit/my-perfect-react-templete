0ee5d954ce3718acbac020c1ecf48e06ff21d830
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-03-09 12:42:30 +0800

    1. 添加less支持成功 2. 引入ant成功, 并且成功使用craco配置文件成功配置主题色

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,19 @@
 /* craco.config.js */
 
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
-}
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { '@primary-color': '#1DA57A' }, // 配置主题色为绿色
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
