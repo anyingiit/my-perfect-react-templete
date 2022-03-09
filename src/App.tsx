7a2b7c7eec7926c18d9e2e8797f58d4291832e9d
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-03-09 20:08:50 +0800

    成功配置tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.less';
-import { Button } from 'antd'
+// import { Button } from 'antd'
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary" className='ant-btn'>Button</Button>
+      {/* <Button type="primary" className='ant-btn'>Button</Button> */}
+      <h1 className='underline text-5xl text-teal-400'>Hello World</h1>
     </div>
   );
 }
